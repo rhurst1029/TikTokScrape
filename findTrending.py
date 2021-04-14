60121129f3a0b55d30bfb512bf6649e8670448b7
Ryan Hurst <ryanhurst@Ryans-MBP.localdomain>
2021-04-13 20:11:18 -0700

    receiving 1/3 of requested data

--- findTrending.py
+++ findTrending.py
@@ -1,5 +1,3 @@
-### This script finds the top trending songs on tiktok and stores them in the topSongs.json file w/ the following structure
-
 from TikTokApi import TikTokApi
 import string
 import random
@@ -10,27 +8,49 @@ verifyFp = 'verify_kngd0n5c_MNpHdxYL_m64K_4y4O_Aejf_xQKslqaYxk13'
 
 did = ''.join(random.choice(string.digits) for num in range(19))
 
-# api = TikTokAPI.get_instance(custom_verifyFp = verifyFp)
+api = TikTokApi.get_instance(custom_verifyFp = verifyFp)
 
 # When fp stops working:
 
 # THIS LINE
-api = TikTokApi.get_instance(custom_verifyFp = verifyFp, use_test_endpoints = True, custom_did = did)
+# api = TikTokApi.get_instance(custom_verifyFp = verifyFp, use_test_endpoints = True, custom_did = did)
 
-#THIS LINE
-# Ryan's code
-h = api.discoverMusic(language='en', proxy=None)
 
-ids = []
-for i in h:
-    ids.append(i['cardItem']['id'])
-id_dict = {}
-for i in ids:
-    #### Gets top tiktoks made with the trending sound IDs ###
-    n = api.by_sound( i, count=30, offset=0)
-    # print(n)
+# Trending
+ids = {}
+count = 300
+trending = api.by_trending(count = count)
 
-    id_dict[i] = []
-    for j in n:
-        id_dict[i].append(j['authorStats'])
-print(id_dict)
+for i in trending:
+    if i.get('music')['title'] == 'original sound':
+        continue
+    else:
+        ids[ i.get('music')['id']] = i.get('music')['title']
+
+
+
+ids2 = {}
+## Filtered is the number of sounds_ids from 'count' (300 rn) that weren't original sounds
+filtered = 0
+## Artist_author is the number of filtered sound ids that have artis/author data
+artist_author = 0
+missing_artist = 0
+for n in ids:
+    music = api.get_music_object_full(n)
+    # ids2[music['music']['id']] = []
+    # ids2[music['music']['id']].append(music['music']['title'
+
+    if music.get('author'):
+        artist_author += 1
+        # print(music)
+    elif music.get('artist'):
+        artist_author += 1
+        # print('artist', '\n','\n','\n', music)
+    else:
+        missing_artist += 1
+        # print('NEITHER', '\n','\n','\n', music)
+filtered += 1
+    
+print('Total_original/300', count - filtered)
+print('total_w_artist_data', artist_author)
+print('total_without_artist_data', missing_artist)
